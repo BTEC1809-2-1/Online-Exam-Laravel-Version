1f7098d99a2b060aa225e613472cca6b38e89952
binhandev <lvba2102@gmail.com>
2020-10-19 22:39:56 +0700

    Create schedule task to update exam status

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,4 +65,6 @@ class Kernel extends HttpKernel
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'admin' => \App\Http\Middleware\Authorize::class,
     ];
+
+
 }
